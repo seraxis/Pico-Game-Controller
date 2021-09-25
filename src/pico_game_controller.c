50396f5b8bf397fb5e0a177c6c77b63dfecde7a2
Nicholas G <speeedypotato@gmail.com>
2021-09-25 01:29:48 -0400

    Pocket IIDX Configuration

--- src/pico_game_controller.c
+++ src/pico_game_controller.c
@@ -90,7 +90,6 @@ void update_lights() {
 struct report {
   uint16_t buttons;
   uint8_t joy0;
-  uint8_t joy1;
 } report;
 
 /**
@@ -109,7 +108,6 @@ void joy_mode() {
     }
 
     report.joy0 = ((double)cur_enc_val[0] / ENC_PULSE) * (UINT8_MAX + 1);
-    report.joy1 = ((double)cur_enc_val[1] / ENC_PULSE) * (UINT8_MAX + 1);
 
     tud_hid_n_report(0x00, REPORT_ID_JOYSTICK, &report, sizeof(report));
   }
@@ -147,8 +145,7 @@ void key_mode() {
       tud_hid_n_report(0x00, REPORT_ID_KEYBOARD, &nkro_report,
                        sizeof(nkro_report));
     } else {
-      tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, delta[0] * MOUSE_SENS,
-                           delta[1] * MOUSE_SENS, 0, 0);
+      tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, delta[0] * MOUSE_SENS, 0, 0, 0);
     }
     // Alternate reports
     kbm_report = !kbm_report;
